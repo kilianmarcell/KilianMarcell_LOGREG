7b92a5e518ddb14e4bca9999fb7da0d3e219a303
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-01 20:00:52 +0100

    Regisztráció gomb kész

---
+++ app/src/main/java/hu/petrik/logreg/DBHelper.java
@@ -0,0 +1,4 @@
+package hu.petrik.logreg;
+
+public class DBHelper {
+}
--- app/src/main/java/hu/petrik/logreg/MainActivity.java
+++ app/src/main/java/hu/petrik/logreg/MainActivity.java
@@ -2,13 +2,38 @@ package hu.petrik.logreg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText felhasznaloEditTextMain, jelszoEditTextMain;
+    private Button bejelentkezesButtonMain, regisztracioButtonMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        regisztracioButtonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void init() {
+        felhasznaloEditTextMain = findViewById(R.id.felhasznaloEditTextMain);
+        jelszoEditTextMain = findViewById(R.id.jelszoEditTextMain);
+        bejelentkezesButtonMain = findViewById(R.id.bejelentkezesButtonMain);
+        regisztracioButtonMain = findViewById(R.id.regisztracioButtonMain);
     }
 }
