ec707c8489a8dcf6fbe8001953a6362f4cfe6d83
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-12-01 20:04:23 +0100

    Adatbázisban adatok felvéve

--- app/src/main/java/hu/petrik/logreg/DBHelper.java
+++ app/src/main/java/hu/petrik/logreg/DBHelper.java
@@ -1,9 +1,24 @@
 package hu.petrik.logreg;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "felhasznaloDB.db";
+    private static final int DB_VERSION = 1;
+
+    private static final String TABLE_NAME = "felhasznalo";
+    private static final String COL_ID = "id";
+    private static final String COL_EMAIL = "email";
+    private static final String COL_FELHNEV = "felhnev";
+    private static final String COL_JELSZO = "jelszo";
+    private static final String COL_TELJESNEV = "teljesnev";
+
+    public DBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
 
